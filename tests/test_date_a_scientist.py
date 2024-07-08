4d41a6af3452f2e09edd5e1848e0b992156a5a9d
Maciej Sobczak <964938+maciejATcosphere@users.noreply.github.com>
2024-07-08 19:04:49 +0200

    add easier data reading

--- tests/test_date_a_scientist.py
+++ tests/test_date_a_scientist.py
@@ -1,3 +1,5 @@
+from unittest.mock import call
+
 import pandas as pd
 import pytest
 from pandas.testing import assert_frame_equal
@@ -27,6 +29,29 @@ class TestDateAScientist(BaseTestCase):
         # WHEN
         # THEN
         assert ds.chat("What is the name of the first person?") == "Alice"
+
+    def test_data_scientist__read_df_from_url(self):
+        # GIVEN
+        read_csv = self.mocker.patch.object(pd, "read_csv")
+        read_csv.return_value = pd.DataFrame(
+            [
+                {"name": "Alice", "age": 25, "city": "New York"},
+                {"name": "Bob", "age": 30, "city": "Los Angeles"},
+                {"name": "Charlie", "age": 35, "city": "Chicago"},
+            ]
+        )
+
+        # WHEN
+        ds = DateAScientist(
+            df="http://some.data/here.csv?sep=%3B&encoding=utf-8",
+            llm_openai_api_token=self.openai_api_token,
+        )
+
+        # THEN
+        assert ds.chat("What is the name of the first person?") == "Alice"
+        assert read_csv.call_args_list == [
+            call('http://some.data/here.csv?sep=%3B&encoding=utf-8', encoding='utf-8', sep=';')
+        ]
 
     def test_data_scientist__with_column_descriptions(self):
         # GIVEN
